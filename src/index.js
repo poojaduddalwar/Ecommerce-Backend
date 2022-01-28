2e261ce079f1589588da694c6088648efbb18adf
Pooja Girish Duddalwar <poojaduddalwar02@gmail.com>
2022-01-29 00:56:04 +0530

    setup complete for server

--- src/index.js
+++ src/index.js
@@ -1,7 +1,19 @@
 import express from "express";
+import dotenv from 'dotenv';
+
+dotenv.config()
+//this makes sure that the environment variable is loaded before the process runs up
+//as the .env file is in the root we don't need to specify the path of the file 
+//otherwise we'll have to specify the path of the .env file like :- dotenv.config("../.env")
 
 const app = express()
 
-app.listen(3000, (req, res) => {
-    console.log("Server listening at PORT 3000")
+const port = process.env.PORT || 3000
+
+console.log(process.env.DB_URL)
+
+// process.stdout.write("hello standard output")
+
+app.listen(port, (req, res) => {
+    console.log(`Server listening at PORT ${port}`)
 })
