3812d2ae7fff0bca93096e7f344025c477df56e2
Pooja Girish Duddalwar <poojaduddalwar02@gmail.com>
2025-07-03 00:04:35 +0530

    fixes finally 5

--- src/controllers/authController.js
+++ src/controllers/authController.js
@@ -4,15 +4,15 @@ import jwt from "jsonwebtoken";
 
 export const signup = async (req, res, next) => {
   try {
-    const { email, password } = req.body;
+    const {name, email, password } = req.body;
     const existingUser = await User.findOne({ email });
     if (existingUser) return res.status(400).json({ message: "User already exists" });
 
     const hashedPassword = await bcrypt.hash(password, 10);
-    const newUser = await User.create({ email, password: hashedPassword });
+    const newUser = await User.create({ name, email, password: hashedPassword, role: "user" });
 
     const token = jwt.sign(
-      { _id: newUser._id, email: newUser.email, role: newUser.role },
+      { _id: newUser._id, name: newUser.name, email: newUser.email, role: newUser.role },
       process.env.JWT_SECRET,
       { expiresIn: "1h" }
     );
